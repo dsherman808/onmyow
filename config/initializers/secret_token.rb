b744b17991f08a6d0b233178ca257d7f19f1306c
d <d@Karis-MacBook-Air.local>
2014-01-14 22:05:31 -1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Onmyow::Application.config.secret_key_base = 'a269ab7522163b3cd284a0ce4cd4fcf71144adf8a304119a56a7bc0d00a46719eeb7b0e4cd3bfefd4da078e7daf46c65fb7bc4a770e902eb71ec3ce81e8eae00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Onmyow::Application.config.session_store :cookie_store, key: '_onmyow_session'
